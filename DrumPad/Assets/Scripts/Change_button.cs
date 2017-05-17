3f979b701fa5211320144996f59fd68cefc88984
MaHyunMin <mahyunm@naver.com>
2017-05-17 15:36:19 +0900

    Unity Tutorials

---
+++ DrumPad/Assets/Scripts/Change_button.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_button : MonoBehaviour {
+
+    Vector3 v;
+
+    public void back_button()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void left_button()
+    {
+        Debug.Log("left");
+        if (transform.position.z == 0)
+        {
+            v = transform.position;
+            v.z = 2;
+            transform.position = v;
+        }
+        else if (transform.position.z == 1)
+        {
+            v = transform.position;
+            v.z = 0;
+            transform.position = v;
+        }
+        else if (transform.position.z == 2)
+        {
+            v = transform.position;
+            v.z = 1;
+            transform.position = v;
+        }
+    }
+
+    public void right_button()
+    {
+        if (transform.position.z == 0)
+        {
+            v = transform.position;
+            v.z = 1;
+            transform.position = v;
+        }
+        else if (transform.position.z == 1)
+        {
+            v = transform.position;
+            v.z = 2;
+            transform.position = v;
+        }
+        else if (transform.position.z == 2)
+        {
+            v = transform.position;
+            v.z = 0;
+            transform.position = v;
+        }
+    }
+}
---
+++ DrumPad/Assets/Scripts/menu_scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void SceneStart()
+    {
+        SceneManager.LoadScene("DrumPad");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
