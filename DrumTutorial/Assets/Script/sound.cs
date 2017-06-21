6592f207b76052e3449497524de0e3bf8080a288
MaHyunMin <mahyunm@naver.com>
2017-06-21 15:19:33 +0900

    modify android to unity

---
+++ DrumTutorial/Assets/Script/sound.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour {
+
+    public SpriteRenderer sr;
+    public Sprite curr;
+    public Sprite next;
+    public AudioClip audio;
+
+    private Touch touch;
+    private Vector3 pos;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if(Input.touchCount > 0)
+        {
+            for(int i = 0; i < Input.touchCount; i++)
+            {
+                touch = Input.GetTouch(i);
+                if(touch.phase == TouchPhase.Began)
+                {
+                    pos = Camera.main.ScreenToWorldPoint(touch.position);
+                    GetComponent<AudioSource>().clip = audio;
+                    GetComponent<AudioSource>().PlayOneShot(audio);
+                    sr.sprite = next;
+                    break;
+                }
+                else if (Input.GetTouch(i).phase == TouchPhase.Moved)
+                {
+                    sr.sprite = next;
+                }
+                else if (Input.GetTouch(i).phase == TouchPhase.Canceled)
+                {
+                    sr.sprite = curr;
+                }
+                else if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                {
+                    sr.sprite = curr;
+                }
+            }
+        }
+        */
+    }
+
+    public void OnMouseDown()
+    {
+        GetComponent<AudioSource>().clip = audio;
+        GetComponent<AudioSource>().PlayOneShot(audio);
+        sr.sprite = next;
+    }
+
+    public void OnMouseUp()
+    {
+        sr.sprite = curr;
+    }
+
+    public void OnMouseDrag()
+    {
+        sr.sprite = next;
+    }
+}
---
+++ DrumTutorial/Assets/Script/tutorial1_1.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tutorial1_1 : MonoBehaviour {
+
+    private GameObject note;
+    private GameObject name;
+    float[] vy = new float[4] { 1.5f, 1.5f, 1.5f, 1.3f }; // crash, hihat1, hihat2, ride
+    int i = 0;
+    Vector3 v;
+
+    Sprite sp;
+    Sprite note_sp;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (note == null)
+        {
+            note = GameObject.FindGameObjectWithTag("Note");
+        }
+
+        if (name == null)
+        {
+            name = GameObject.FindGameObjectWithTag("Name");
+        }
+
+        v = note.transform.localPosition;
+        v.x = 0;
+        v.y = vy[0];
+        v.z = -1;
+
+        sp = (Sprite)Resources.Load("Image/name" + (i+11), typeof(Sprite));
+        name.GetComponent<SpriteRenderer>().sprite = sp;
+
+        note_sp = (Sprite)Resources.Load("Image/note" + (i + 11), typeof(Sprite));
+        note.GetComponent<SpriteRenderer>().sprite = note_sp;
+    }
+
+    public void backScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void leftClick()
+    {
+        if (i == 0)
+        {
+            i = 3;
+            v.y = vy[i];
+            note.transform.position = v;
+            note_sp = (Sprite)Resources.Load("Image/note" + (i + 11), typeof(Sprite));
+            note.GetComponent<SpriteRenderer>().sprite = note_sp;
+            sp = (Sprite)Resources.Load("Image/name" + (i + 11), typeof(Sprite));
+            name.GetComponent<SpriteRenderer>().sprite = sp;
+        }
+        else
+        {
+            i--;
+            v.y = vy[i];
+            note.transform.position = v;
+            note_sp = (Sprite)Resources.Load("Image/note" + (i + 11), typeof(Sprite));
+            note.GetComponent<SpriteRenderer>().sprite = note_sp;
+            sp = (Sprite)Resources.Load("Image/name" + (i + 11), typeof(Sprite));
+            name.GetComponent<SpriteRenderer>().sprite = sp;
+        }
+    }
+
+    public void rightClick()
+    {
+        if (i == 3)
+        {
+            i = 0;
+            v.y = vy[i];
+            note.transform.position = v;
+            note_sp = (Sprite)Resources.Load("Image/note" + (i + 11), typeof(Sprite));
+            note.GetComponent<SpriteRenderer>().sprite = note_sp;
+            sp = (Sprite)Resources.Load("Image/name" + (i + 11), typeof(Sprite));
+            name.GetComponent<SpriteRenderer>().sprite = sp;
+        }
+        else
+        {
+            i++;
+            v.y = vy[i];
+            note.transform.position = v;
+            note_sp = (Sprite)Resources.Load("Image/note" + (i + 11), typeof(Sprite));
+            note.GetComponent<SpriteRenderer>().sprite = note_sp;
+            sp = (Sprite)Resources.Load("Image/name" + (i + 11), typeof(Sprite));
+            name.GetComponent<SpriteRenderer>().sprite = sp;
+        }
+    }
+}
